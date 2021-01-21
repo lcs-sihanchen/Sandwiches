3dab83b97265cf7df790f0a56d824f5411d45d90
Chen Sihan <schen@lcs.on.ca>
2021-01-21 01:01:56 -0500

    Check if the sandwiches are spicy

--- Shared/SandwichDetail.swift
+++ Shared/SandwichDetail.swift
@@ -16,20 +16,36 @@ struct SandwichDetail: View {
     
     @State private var zoomed = false
     var body: some View {
-        Image(sandwich.imageName)
-            .resizable()
-            // If zoomed = false, .fit, and vice versa
-            .aspectRatio(contentMode: zoomed ? .fill : .fit)
-            .onTapGesture {
-                // Animate between different state
-                withAnimation {
-                    zoomed.toggle()
+        VStack {
+            Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+            Image(sandwich.imageName)
+                .resizable()
+                // If zoomed = false, .fit, and vice versa
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .onTapGesture {
+                    // Animate between different state
+                    withAnimation {
+                        zoomed.toggle()
+                    }
                 }
-                
+            Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+            if sandwich.isSpicy {
+                HStack {
+                    Spacer()
+                    Label("Spicy", systemImage: "flame.fill")
+                    Spacer()
+                }
+                .padding(.all)
+                .font(Font.headline.smallCaps())
+                .background(Color.red)
+                .foregroundColor(.yellow)
+                   
             }
-            .navigationTitle(sandwich.name)
-            // Fill the whole screen
-            .edgesIgnoringSafeArea(.bottom)
+            }
+           
+        .navigationTitle(sandwich.name)
+        // Fill the whole screen
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
@@ -39,8 +55,13 @@ struct SandwichDetail: View {
 
 struct SandwichDetail_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView{
-            SandwichDetail(sandwich: testData[0])
+        Group {
+            NavigationView{
+                SandwichDetail(sandwich: testData[0])
+            }
+            NavigationView{
+                SandwichDetail(sandwich: testData[1])
+            }
         }
        
     }
